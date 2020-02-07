6e559c9edcfd70cb8585ea6547fae2ca938aa830
Doug Ritchie <dritchie3@sbcglobal.net>
2020-02-07 12:31:51 -0600

    Math Generator works now.

--- script.js
+++ script.js
@@ -13,65 +13,103 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+// prompt/confirm length of password and what characters type to use. 
+    //  windoonload function to make sure html loads first.
+    window.onload = function(){
+      var length = prompt("Enter your password size. It must be between 8 and 128.", "0");
+      var upper = confirm('I would like Uppercase in my password. Please click (OK) for Yes or (Cancel) for No.');
+      var lower = confirm('I would like Lowercase in my password. Please click (OK) for Yes or (Cancel) for No.');
+      var number = confirm('I would like Numbers in my password. Please click (OK) for Yes or (Cancel) for No.');
+      var special = confirm('I would like special characters in my password. Please click (OK) for Yes or (Cancel) for No.');
+      var typecount= 0;
+      var letter;
+      var letters = [];
+      
+      
+
+      // This come up with the count of the types
+      if (upper === true) {
+          (typecount++);
+      }
+      if (lower === true){          
+          typecount++;
+      }
+      if (number === true) {        
+        typecount++;
+      }
+
+      if (special === true) {          
+        typecount++;
+      }
 
 
 
-// Character Generator Functions
-function getRandomUpper(){
-  return String.fromCharCode(Math.floor(Math.random()*26)+65);
-}
-function getRandomLower(){
-  return String.fromCharCode(Math.floor(Math.random()*26)+97);
-}
-function getRandomNumber(){
-  return String.fromCharCode(Math.floor(Math.random()*10)+48);
-}
-function getRandomSpecial(){
-  const special = '!@#$%^&*()_+-=<>[]{}|/?><,.';
-  return special[Math.floor(Math.random() * special.length)]
-}
+      // loop to keep choosing characters for the full length of the password
+      for (l = 0; l < length ; l++) { 
+        // var charType = [];
+        
+        // create random characters for the type sets chosen 
+        for (i = 0; i < typecount; i++){
+          letters.push(letter);
+          
+          charType = [];
+          // Character Generator Functions
+          function getRandomUpper(){
+            return String.fromCharCode(Math.floor(Math.random()*26)+65);
+          }
+          function getRandomLower(){
+            return String.fromCharCode(Math.floor(Math.random()*26)+97);
+          }
+          function getRandomNumber(){
+            return String.fromCharCode(Math.floor(Math.random()*10)+48);
+          }
+          function getRandomSpecial(){
+            const special = '!@#$%^&*()_+-=<>[]{}|/?><,.';
+            return special[Math.floor(Math.random() * special.length)]
+          }
 
-// prompt/confirm length of password and what characters too unescape.
-
-window.onload = function(){
-var length = prompt("Enter your password size. It must be between 8 and 128.", "0");
-var upper = confirm('I would like Uppercase in my password. Please click (OK) for Yes or (Cancel) for No.');
-var lower = confirm('I would like Lowercase in my password. Please click (OK) for Yes or (Cancel) for No.');
-var number = confirm('I would like Numbers in my password. Please click (OK) for Yes or (Cancel) for No.');
-var special = confirm('I would like special characters in my password. Please click (OK) for Yes or (Cancel) for No.');
-var charType = []
-
-
-
-// Identify which characters to use
-
-if (upper === true) {
-    charType.push(getRandomUpper());
-}
-if (lower === true){
-    charType.push(getRandomLower());
-}
-if (number === true) {
-  charType.push(getRandomNumber());
-}
-
-if (special === true) {  
-  charType.push(getRandomSpecial());
-}
- 
-
-// Choose Random Characters from functions
-
-for (l = 0; l < length ; l++) {  
-  for (i = 0; i < length; i++){
-      letter = charType[Math.floor(Math.random() * charType.length)]; 
-  }
-  console.log(letter)  
-  console.log(charType)
-}
-}
+          
 
 
+          // Identify which characters to use
+
+          if (upper === true) {
+              charType.push(getRandomUpper());
+          }
+          if (lower === true){
+              charType.push(getRandomLower());
+          }    
+          if (number === true) {
+            charType.push(getRandomNumber());     
+          }
+          if (special === true) {  
+            charType.push(getRandomSpecial());      
+          } 
+
+          this.console.log(charType)
+          letter = charType[Math.floor(Math.random() * charType.length)]; 
+          // for (a=0; a < length; a++){
+          //   letters[a] = letter;
+
+          // }  
+      
+          // if (upper === true || lower === true || number === true || special === true) {
+            // letters.push(letter());
+
+          console.log(letter);  
+          
+
+          // let output = document.getElementById("letters");
+          // output.textContent = letters;  
+        
+          // code to print letters 
+          // function password() {
+          //   document.getElementById("letter").innerHTML = password
+          // }
+        }
+      }
+      document.getElementById("password").innerHTML = letter;
+    }   
 
 
 
@@ -85,10 +123,8 @@ for (l = 0; l < length ; l++) {
 
 
 
-// var upperCase = ('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z')
-// var lowerCase = ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
-// var numbers = ('0','1','2','3','4','5','6','7','8','9')
-// var special = ('!','@','#','$','%','^','&','*','(',')','_','+','-','=','?','/',';',':','|','<','>')
+
+
 
 
 
